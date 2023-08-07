04396c2018270e57151962fd8afb320f7b146e63
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-08 00:51:30 +0530

    4 th commit

--- tempPrint.java
+++ tempPrint.java
@@ -19,7 +19,23 @@ public class tempPrint {
         if(avgMarks >= 75.0) status = "\033[34;1mDistinguished Pass\033[0m\t\t\t\t\t\t\t";
         else if(avgMarks >= 65.0) status = "\033[32;1mCredit Pass\033[0m\t\t\t\t\t\t\t";
         else if(avgMarks >= 55.0) status = "\033[33;1mPass \033[0m\t\t\t\t\t\t\t\t"; 
-        else status ="\033[31;1mFail\033[0m\t\t\t\t\t\t\t\t";    
+        else status ="\033[31;1mFail\033[0m\t\t\t\t\t\t\t\t"; 
+        
+        String result;
+        if(marks1 >= 75.0) result = "\033[34;1mDistinguished Pass\033[0m";
+        else if(marks1 >= 65.0) result = "\033[32;1mCredit Pass\033[0m";
+        else if(marks1 >= 55.0) result = "\033[33;1mPass \033[0m"; 
+        else result ="\033[31;1mFail\033[0m"; 
+
+        if(marks2 >= 75.0) result = "\033[34;1mDistinguished Pass\033[0m";
+        else if(marks2 >= 65.0) result = "\033[32;1mCredit Pass\033[0m";
+        else if(marks2 >= 55.0) result = "\033[33;1mPass \033[0m"; 
+        else result ="\033[31;1mFail\033[0m"; 
+
+        if(marks3 >= 75.0) result = "\033[34;1mDistinguished Pass\033[0m";
+        else if(marks3 >= 65.0) result = "\033[32;1mCredit Pass\033[0m";
+        else if(marks3 >= 55.0) result = "\033[33;1mPass \033[0m"; 
+        else result ="\033[31;1mFail\033[0m"; 
         
         String subjectCode1 = sub1.substring(3);
 
@@ -33,7 +49,7 @@ public class tempPrint {
         System.out.printf("|   Total : %.2f \t\t\t\t\t Avg : %.2f%s\t\t|\n",totalMarks,avgMarks,"%");
         System.out.println("+"+"-".repeat(79)+"+");
         System.out.println("|\tSubject\t\t|\tMarks\t\t|\t\tStatus\t\t|");
-        System.out.printf("|\tSE-00%s\t\t|",subjectCode1);
+        System.out.printf("|\tSE-00%s\t\t|\t%.2f\t\t|\t\t%s\t\t|\n",subjectCode1,marks1,result);
 
 
     }
