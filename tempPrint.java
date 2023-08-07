eab0013a0c35cc378990649ec5e38abe4e05685b
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-08 00:36:00 +0530

    3 rd commit

--- tempPrint.java
+++ tempPrint.java
@@ -2,21 +2,26 @@ public class tempPrint {
     public static void main(String[] args) {
         String name = "Gayum sasiri";
         int age = 15;
-        double sub1 =10.34343;
-        double sub2 =45.33343;
-        double sub3 = 34.99434;
+        double marks1 =10.34343;
+        double marks2 =45.33343;
+        double marks3 = 34.99434;
+        String sub1 ="SE-1";
+        String sub2 ="SE-2";
+        String sub3 ="SE-3";
 
 
 
 
 
-        double totalMarks = sub1 + sub2 + sub3;
+        double totalMarks = marks1 + marks2 + marks3;
         double avgMarks = totalMarks/3.0;
         String status;
         if(avgMarks >= 75.0) status = "\033[34;1mDistinguished Pass\033[0m\t\t\t\t\t\t\t";
         else if(avgMarks >= 65.0) status = "\033[32;1mCredit Pass\033[0m\t\t\t\t\t\t\t";
         else if(avgMarks >= 55.0) status = "\033[33;1mPass \033[0m\t\t\t\t\t\t\t\t"; 
-        else status ="\033[31;1mFail\033[0m\t\t\t\t\t\t\t\t";      
+        else status ="\033[31;1mFail\033[0m\t\t\t\t\t\t\t\t";    
+        
+        String subjectCode1 = sub1.substring(3);
 
         
         String nameUpperCase = name.toUpperCase();
@@ -27,6 +32,9 @@ public class tempPrint {
         System.out.printf("|   Status : %s|\n" , status);
         System.out.printf("|   Total : %.2f \t\t\t\t\t Avg : %.2f%s\t\t|\n",totalMarks,avgMarks,"%");
         System.out.println("+"+"-".repeat(79)+"+");
+        System.out.println("|\tSubject\t\t|\tMarks\t\t|\t\tStatus\t\t|");
+        System.out.printf("|\tSE-00%s\t\t|",subjectCode1);
+
 
     }
     
